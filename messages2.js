5ed66e1b0405847c23c637de3c62994b72064cd9
VictorGorban <VictorGorban2@ya.ru>
2019-02-20 00:02:45 +0300

    Renamed files. Hoping nothing will change, that was unused files

---
+++ messages2.js
@@ -0,0 +1,67 @@
+opera.isReady(function() {
+  var messages = window["messages"] = {
+    'language': 'ru',
+    'strings': {
+      'titleMain': {
+        defaultText: 'Перевести',
+        localText: 'Перевести'
+      },
+      'titleDefaultLang': {
+        defaultText: 'Язык по умолчанию',
+        localText: 'Язык по умолчанию'
+      },
+      'titleGeneral': {
+        defaultText: 'Общие настройки',
+        localText: 'Общие настройки'
+      },
+      'titleEnableAutoPrompt': {
+        defaultText: 'Показывать подсказку на страницах',
+        localText: 'Показывать подсказку на страницах'
+      },
+      'titleEnableAutoTranslation': {
+        defaultText: 'Автоматически переводить после вывода подсказки',
+        localText: 'Автоматически переводить после вывода подсказки'
+      },
+      'manualTranslationNote': {
+        defaultText: 'Если вы выберете "Нет", то появится кнопка для запуска перевода',
+        localText: 'Если вы выберете "Нет", то появится кнопка для запуска перевода'
+      },
+      'titleEnableSecureTranslation': {
+        defaultText: 'Перевод Защищенных страниц',
+        localText: 'Перевод Защищенных страниц'
+      },
+      'titleLanguageSettings': {
+        defaultText: 'Изменить настройки для каждого языка отдельно',
+        localText: 'Изменить настройки для каждого языка отдельно'
+      },
+      'clearPreferences': {
+        defaultText: 'Сбросить все настройки',
+        localText: 'Сбросить все настройки'
+      },
+      'blanketPreferences': {
+        defaultText: 'Переводить со всех языков',
+        localText: 'Переводить со всех языков'
+      },
+      'blanketPreferencesOff': {
+        defaultText: 'Никогда не переводить',
+        localText: 'Никогда не переводить'
+      },
+      'translateAlwaysOption': {
+        defaultText: 'Всегда переводить с этого языка',
+        localText: 'Всегда переводить с этого языка'
+      },
+      'translateNeverOption': {
+        defaultText: 'Никогда не переводить с этого языка',
+        localText: 'Никогда не переводить с этого языка'
+      },
+      'yes': {
+        defaultText: 'Да',
+        localText: 'Да'
+      },
+      'no': {
+        defaultText: 'Нет',
+        localText: 'Нет'
+      }
+    }
+  };
+});
